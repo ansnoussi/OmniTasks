05b903df70e54531bceccaba1ca31225d5ef931d
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-01-05 03:31:23 +0100

    shared theme

--- omnitasks/jest.config.js
+++ omnitasks/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/omnitasks',
     '<rootDir>/apps/api',
     '<rootDir>/apps/omnitasks-mobile',
+    '<rootDir>/libs/theme',
   ],
 };
---
+++ omnitasks/libs/theme/src/lib/theme.ts
@@ -0,0 +1,12 @@
+
+export const colors = {
+  main: "#3188dd",
+  accent: "#ff3562",
+  background_dark : "#131150",
+  background_light : "#2d2c70",
+  green : "#90fea7",
+  orange : "#f7931a",
+  blue : "#3457fd",
+  yellow : "#ffc702",
+  light : "#acc5e5",
+};
