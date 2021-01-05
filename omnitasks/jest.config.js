5d34bf7e32938dc081b7cf1b9fd47bb7682f113b
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-01-05 01:52:11 +0100

    created mobile app

--- omnitasks/jest.config.js
+++ omnitasks/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/omnitasks', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/omnitasks',
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/omnitasks-mobile',
+  ],
 };
